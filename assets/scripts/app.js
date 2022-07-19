36c1fa020613e81ffbcad0a86b5d9c0735267032
Maxx Crawford <maxx.crawford@gmail.com>
2022-07-18 22:34:56 -0500

    Add footer icons, logic to scroll around

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,62 +1,106 @@
-(function() {
-	"use strict";
+(function () {
+  "use strict";
 
-	const rotator = document.querySelector(".rotator section");
+  const rotatorSections = document.querySelector(".rotator section");
+  const arrowDownBtn = document.getElementById("arrowDown");
 
-    const arrowDownBtn = document.getElementById("arrowDown");
+  arrowDownBtn.addEventListener(
+    "click",
+    () => {
+      rotatorSections.scrollIntoView({ behavior: "smooth" });
+    },
+    false
+  );
 
-    arrowDownBtn.addEventListener("click", ()=> {
-        rotator.scrollIntoView({behavior: "smooth"});
-    }, false)
-    
-    // console.log("rotator:", rotator);
+  const arrowDownSectionBtn = document.querySelectorAll(".js-arrow-down");
 
-    const arrowDownSectionBtn = document.querySelectorAll(".js-arrow-down");
-    
-    arrowDownSectionBtn.forEach( el => {
-        el.addEventListener("click", ()=>{
-            const parentSection = el.parentElement.closest('section');
-            const nextSection = parentSection.nextElementSibling;
-            if (nextSection === null ) {
-                const whoSection = document.getElementById("who");
-                whoSection.scrollIntoView({behavior: "smooth"});
-                return;
-            }
-            nextSection.scrollIntoView({behavior: "smooth"});
-        }, false);
-    });
-
-    let observerOptions = {
-        rootMargin: '0px',
-        threshold: 0.5
-    }
-    
-    const observer = new IntersectionObserver(observerCallback, observerOptions);
-    
-    const sideNavLinks = document.querySelectorAll(".side-nav a");
-
-    
-
-    function observerCallback(entries, observer) {
-        entries.forEach(entry => {
-            if(entry.isIntersecting) {
-                const filterHref = "#" + entry.target.id;
-                const targetLink = [...sideNavLinks].filter(el => { 
-                    return el.hash == filterHref;
-                 });
-
-                 document.querySelector(".side-nav a.active").classList.remove("active");
-                 targetLink[0].classList.add("active");
-              //do something
-            }
-        });
-    };
-    
-    document.querySelectorAll(".projects section").forEach((i) => {
-        if (i) {
-            observer.observe(i);
+  arrowDownSectionBtn.forEach((el) => {
+    el.addEventListener(
+      "click",
+      () => {
+        const parentSection = el.parentElement.closest("section");
+        const nextSection = parentSection.nextElementSibling;
+        if (nextSection === null) {
+          const whoSection = document.getElementById("who");
+          whoSection.scrollIntoView({ behavior: "smooth" });
+          return;
         }
+        nextSection.scrollIntoView({ behavior: "smooth" });
+      },
+      false
+    );
+  });
+
+  let observerOptions = {
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  let observerOption2 = {
+    rootMargin: "0px",
+    threshold: 0.75,
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  const sideNavLinks = document.querySelectorAll(".side-nav a");
+
+  function observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const filterHref = "#" + entry.target.id;
+        const targetLink = [...sideNavLinks].filter((el) => {
+          return el.hash == filterHref;
+        });
+        entry.target.scrollIntoView({ behavior: "smooth" });
+        document.querySelector(".side-nav a.active").classList.remove("active");
+        targetLink[0].classList.add("active");
+      }
     });
-    
-      
+  }
+
+  const observer2 = new IntersectionObserver(observerCallback, observerOption2);
+
+  function observerCallback2(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  }
+
+  document.querySelectorAll(".projects section").forEach((i) => {
+    if (i) {
+      observer.observe(i);
+    }
+  });
+
+  const rotator = document.querySelector(".rotator");
+
+  observer2.observe(rotator);
+
+  const people = document.querySelectorAll(".people button");
+
+  people.forEach((el) => {
+    el.addEventListener(
+      "click",
+      (el) => {
+        document
+          .querySelector(".people button.outline")
+          .classList.remove("outline");
+        document
+          .querySelector(".people button.outline-white")
+          .classList.remove("outline-white");
+        el.target.classList.add("outline");
+        el.target.classList.add("outline-white");
+        console.log(el.target.dataset.person);
+        document
+          .querySelector(".bios article.active")
+          .classList.remove("active");
+        document
+          .getElementById(el.target.dataset.person)
+          .classList.add("active");
+      },
+      false
+    );
+  });
 })();
