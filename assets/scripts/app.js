6bf6fdc47b688c5aaec90e107a99c4b7f8cc22d7
Maxx Crawford <maxx.crawford@gmail.com>
2022-07-26 22:47:18 -0500

    Update bio images, section images, add nav for all sections

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,16 @@
 (function () {
   "use strict";
 
-  const rotatorSections = document.querySelector(".rotator section");
-  const arrowDownBtn = document.getElementById("arrowDown");
+  // const rotatorSections = document.querySelector(".rotator section");
+  // const arrowDownBtn = document.getElementById("arrowDown");
 
-  arrowDownBtn.addEventListener(
-    "click",
-    () => {
-      rotatorSections.scrollIntoView({ behavior: "smooth" });
-    },
-    false
-  );
+  // arrowDownBtn.addEventListener(
+  //   "click",
+  //   () => {
+  //     rotatorSections.scrollIntoView({ behavior: "smooth" });
+  //   },
+  //   false
+  // );
 
   const arrowDownSectionBtn = document.querySelectorAll(".js-arrow-down");
 
@@ -21,7 +21,7 @@
         const parentSection = el.parentElement.closest("section");
         const nextSection = parentSection.nextElementSibling;
         if (nextSection === null) {
-          const whoSection = document.getElementById("who");
+          const whoSection = document.getElementById("contact");
           whoSection.scrollIntoView({ behavior: "smooth" });
           return;
         }
@@ -36,10 +36,10 @@
     threshold: 0.5,
   };
 
-  
-
   const observer = new IntersectionObserver(observerCallback, observerOptions);
   const sideNavLinks = document.querySelectorAll(".side-nav a");
+
+  // console.log("sideNavLinks:", sideNavLinks);
 
   function observerCallback(entries, observer) {
     entries.forEach((entry) => {
@@ -48,12 +48,15 @@
         const targetLink = [...sideNavLinks].filter((el) => {
           return el.hash == filterHref;
         });
+
+        // console.log("targetLink: ", targetLink);
+
         if (document.querySelector(".side-nav a.active") && targetLink[0]) {
             document.querySelector(".side-nav a.active").classList.remove("active");
             targetLink[0].classList.add("active");
         }
         
-        console.log("entry.target", entry.target);
+        // console.log("entry.target", entry.target);
         // entry.target.scrollIntoView({ behavior: "smooth" });
       }
     });
@@ -70,7 +73,7 @@
   //   });
   // }
 
-  document.querySelectorAll(".projects section").forEach((i) => {
+  document.querySelectorAll("main section").forEach((i) => {
     if (i) {
       observer.observe(i);
     }
@@ -86,26 +89,26 @@
   //   threshold: 0
   // }
 
-  function handleIntersection(entries) {
-    const rotatorNav = document.querySelector(".rotator .rotator-nav")
-    entries.forEach(entry => {
-      // const intersecting = entry.isIntersecting
-      // entry.target.style.backgroundColor = intersecting ? "blue" : "orange"
-      if (entry.isIntersecting) {
-        rotatorNav.classList.add("fixed");
-        rotatorNav.classList.remove("hidden");
-      } else {
-        console.log("!entry.isIntersecting: ", entry);
-        rotatorNav.classList.remove("fixed");
-        rotatorNav.classList.add("hidden");
+  // function handleIntersection(entries) {
+  //   const rotatorNav = document.querySelector(".rotator .rotator-nav")
+  //   entries.forEach(entry => {
+  //     // const intersecting = entry.isIntersecting
+  //     // entry.target.style.backgroundColor = intersecting ? "blue" : "orange"
+  //     if (entry.isIntersecting) {
+  //       rotatorNav.classList.add("fixed");
+  //       rotatorNav.classList.remove("hidden");
+  //     } else {
+  //       console.log("!entry.isIntersecting: ", entry);
+  //       rotatorNav.classList.remove("fixed");
+  //       rotatorNav.classList.add("hidden");
 
-      }
-    })
-  }
+  //     }
+  //   })
+  // }
 
-  const observerRotator = new IntersectionObserver(handleIntersection);
+  // const observerRotator = new IntersectionObserver(handleIntersection);
   
-  observerRotator.observe(document.querySelector(".rotator"));
+  // observerRotator.observe(document.querySelector(".rotator"));
 
   // const rotator = document.querySelector(".rotator");
 
@@ -125,7 +128,7 @@
           .classList.remove("ring-white");
         el.target.classList.add("ring");
         el.target.classList.add("ring-white");
-        console.log(el.target.dataset.person);
+        // console.log(el.target.dataset.person);
         document
           .querySelector(".bios article.active")
           .classList.remove("active");
